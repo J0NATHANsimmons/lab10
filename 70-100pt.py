1b5fe21a5db3eac9d697d0286ba1b07c984ae387
J0NATHANsimmons <fly727@juno.com>
2014-10-21 17:01:56 -0500

    100 pt. - House(lab 10)

--- 70-100pt.py
+++ 70-100pt.py
@@ -23,6 +23,33 @@ drawpad = Canvas(root, width=800,height=600, background='white')
 drawpad.grid(row=0, column=1)
 
 # Insert your code here to draw the house!
-
-
+#my first beautiful window
+rectangle = drawpad.create_rectangle(260,200,410,540, fill = 'indian red')
+window = drawpad.create_rectangle(280,250,330,320, fill = 'light sky blue')
+#vertical line
+line = drawpad.create_line(280,285,330,285)
+#horizontal line
+line = drawpad.create_line(305,250,305,320)
+#my second beautiful window
+rectangle = drawpad.create_rectangle(350,360,400,440, fill = 'light sky blue')
+#vertical line
+line = drawpad.create_line(375,360,375,440,)
+#horizontal line
+line = drawpad.create_line(350,400,400,400)
+#-------roof------#
+#left side
+line = drawpad.create_line(260,200,330,90)
+#right side
+line = drawpad.create_line(410,200,330,90)
+#chimeny
+line = drawpad.create_line(370,145,370,110)
+line = drawpad.create_line(370,110,390,110)
+line = drawpad.create_line(390,170,390,110)
+#door
+rectangle = drawpad.create_rectangle(320,500,350,540, fill = 'sandy brown')
+#doornob
+circle = drawpad.create_oval(323,520,325,525, fill = 'brown') 
+#grass
+rectangle = drawpad.create_rectangle(0,540,800,700, fill = 'lawn green')
 root.mainloop()
+
